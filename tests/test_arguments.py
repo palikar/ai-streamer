a0905f04676887c141beb8bf7f82f9a97337ea8c
Stanislav Arnaudov <stanislav_ts@abv.bg>
2018-11-20 23:58:19 +0100

    Adding some tests for the argument control

--- tests/test_arguments.py
+++ tests/test_arguments.py
@@ -108,6 +108,56 @@ class TestDown(unittest.TestCase):
 
         with self.assertRaises(SystemExit):
             parsed = arg.parse(arguments)
+
+
+    def test_list_files_1(self):
+        test_file = os.path.join(os.path.dirname(__file__), 'test.txt')
+        test_dir = os.path.join(os.path.dirname(__file__), 'test_dir')
+
+        arguments = ['-c', test_file, '--data', test_dir,
+                     '--list-files', 'list_1', test_file]
+        
+        arg = Argumenter()
+        arg.init("simple")
+        arg.add_list_files(['list_1'])
+        arg.build()
+
+        parsed = arg.parse(arguments)
+
+    
+    def test_list_files_1_no_list(self):
+        test_file = os.path.join(os.path.dirname(__file__), 'test.txt')
+        test_dir = os.path.join(os.path.dirname(__file__), 'test_dir')
+
+        arguments = ['-c', test_file, '--data', test_dir,
+                     '--list-files', 'list_1', test_file]
+        
+        arg = Argumenter()
+        arg.init("simple")
+        arg.build()
+
+        with self.assertRaises(SystemExit):
+            parsed = arg.parse(arguments)
+
+    
+    def test_list_files_2(self):
+        test_file = os.path.join(os.path.dirname(__file__), 'test.txt')
+        test_dir = os.path.join(os.path.dirname(__file__), 'test_dir')
+
+        arguments = ['-c', test_file, '--data', test_dir,
+                     '--list-files', 'list_2', test_file]
+        
+        arg = Argumenter()
+        arg.init("simple")
+        arg.add_list_files(['list_1'])
+        arg.build()
+
+        parsed = arg.parse(arguments)
+        with self.assertRaises(SystemExit):
+            arg.validate(parsed)
+
+
+
         
 
 
